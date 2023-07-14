96e8a4fd5d3577266a21453a79c4499ace826167
hal-wang <hi@hal.wang>
2023-07-14 15:33:43 +0800

    Add project files.

---
+++ CustomDateTimePicker.cs
@@ -0,0 +1,12 @@
+﻿using MahApps.Metro.Controls;
+
+namespace FileTimeEditor
+{
+    public class CustomDateTimePicker : DateTimePicker
+    {
+        protected override string GetValueForTextBox()
+        {
+            return SelectedDateTime?.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using MahApps.Metro.Controls;
+using System.IO;
+using System.Windows;
+
+namespace FileTimeEditor
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var createTime = CreateTime.SelectedDateTime;
+            var updateTime = UpdateTime.SelectedDateTime;
+
+            if (createTime == null && updateTime == null)
+            {
+                MessageBox.Show(this, "请至少选择创建时间或更新时间", "请选择", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog()
+            {
+                Filter = "All files (*.*)|*.*"
+            };
+            var result = openFileDialog.ShowDialog();
+            if (result != true)
+            {
+                return;
+            }
+            var file = openFileDialog.FileName;
+
+            if (updateTime != null)
+            {
+                File.SetLastWriteTime(file, updateTime.Value);
+            }
+            if (createTime != null)
+            {
+                File.SetCreationTime(file, createTime.Value);
+            }
+
+            MessageBox.Show(this, "设置完成", "完成", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
